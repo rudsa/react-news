fa8889dc360f477c3e8d2ce1a872f27c69191815
rudsa <rudsa@naver.com>
2023-05-29 17:50:02 +0900

    react-router-customHook

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,7 +1,7 @@
-import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
 import axios from 'axios';
+import usePromise from '../lib/usePromise';
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -17,38 +17,30 @@ const NewsListBlock = styled.div`
 `;
 
 const NewsList = ({ category }) => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const query = category === 'all' ? '' : `&category=${category}`;
-        const response = await axios.get(
-          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=881fd850147043ea8d57c2ba1448b2f3`,
-        );
-        setArticles(response.data.articles);
-      } catch (e) {
-        console.log(e);
-      }
-      setLoading(false);
-    };
-    fetchData();
+  const [loading, response, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `&category=${category}`;
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=881fd850147043ea8d57c2ba1448b2f3`,
+    );
   }, [category]);
 
   if (loading) {
-    return <NewsListBlock>대기 중...</NewsListBlock>;
+    return <NewsListBlock>대기 중 ...</NewsListBlock>;
   }
 
-  if (!articles) {
+  if (!response) {
     return null;
   }
 
+  if (error) {
+    return <NewsListBlock>에러 발생!</NewsListBlock>;
+  }
+
+  const { articles } = response.data;
   return (
     <NewsListBlock>
       {articles.map((article) => (
-        <NewsItem key={article.url} article={article} />
+        <NewsItem key={article.url} article={article}></NewsItem>
       ))}
     </NewsListBlock>
   );
